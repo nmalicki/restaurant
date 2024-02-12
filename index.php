d3bb59e4048445a8f316bb3a5138f00f25aef659
nmalicki <nmalicki@CompSciICv-47.fsc.int>
2024-02-12 12:01:34 -0500

    Initial Commit

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<!--
+Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+Click nbfs://nbhost/SystemFileSystem/Templates/Scripting/EmptyPHPWebPage.php to edit this template
+-->
+<html>
+    <head>
+        <?php include 'header.php'; ?>
+        
+        <title>Restaurant: Home</title>
+    </head>
+    <body>
+        <!-- A grey horizontal navbar that becomes vertical on small screens -->
+
+        <?php
+        // put your code here
+        ?>
+    </body>
+</html>
---
+++ public.php
@@ -0,0 +1,16 @@
+<html>
+    <head>
+        <?php include 'header.php'; ?>
+    </head>
+    <body>
+        <h1>Public</h1>
+
+    </body>
+</html>
+<?php
+
+/* 
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Scripting/EmptyPHP.php to edit this template
+ */
+
