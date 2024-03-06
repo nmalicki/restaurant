b499902615581a26bdf8496d851241484eb5176c
nmalicki <nmalicki@CompSciICv-10.fsc.int>
2024-03-06 16:03:30 -0500

    changed locations of files again

---
+++ header.php
@@ -0,0 +1,21 @@
+<link rel="stylesheet" href='shared/main.css'>
+<script src="shared/scripts.js"></script>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+
+<nav id="navBar">
+    <div id="contextMenu">
+        <h1><a href="">Restaurant</a></h1>
+        <button>Test 1</button>
+        <button>Test 2</button>
+    </div>
+    <div id="signInIsland">
+        <button><a href="customer/locations.php">Locations</a></button>
+        <button id="signInButton" onclick="signInPopUp()">Sign In</button>
+        <button id="signOut"> Sign Out</button>
+    </div>
+    
+    <div id="signInContainer">
+        <?php include 'customer/signInPopUp.php'; ?>
+    </div>
+</nav>
