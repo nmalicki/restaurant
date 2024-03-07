25f2eef04190555aa573dc94353011ca06207968
nmalicki <nmalicki@CompSciICv-10.fsc.int>
2024-03-06 19:54:49 -0500

    changes to sign in page styling

---
+++ customer/signInPopUp.php
@@ -0,0 +1,56 @@
+<button id="signInContainerClose" onclick="closePopUp('signInContainer')">X</button>
+<div id="toggleDiv">
+    <button id="registerButton">Register</button>
+    <button id="signInHButton">Sign In</button>
+</div>
+
+<form id="registerForm">
+    <div id="registerNames">
+        <div class="half left">
+            <label>Given Name</label>
+            <br>
+            <input></input>
+        </div>
+        <div class="half right">
+            <label>Family Name</label>
+            <br>
+            <input></input>
+        </div>
+    </div>    
+    <label>Email</label>
+    <br>
+    <input></input>
+    <br>
+    <label>Password</label>
+    <br>
+    <input type="password"></input>
+    <br>
+    <label>Confirm password</label>
+    <br>
+    <input type="password"></input>
+    <br>
+    <input type="submit"></input>
+</form>
+
+<form id="logInForm">
+    <label>email</label>
+    <br>
+    <input></input>
+    <br>
+    <label>Password</label>
+    <br>
+    <input></input>
+    <br>
+    <br>
+    <input type="submit"></input>
+</form>
+
+
+
+<?php
+
+/* 
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Scripting/EmptyPHP.php to edit this template
+ */
+
