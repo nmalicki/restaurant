fcd3e88dc95f6962b41f0013ad8f17ed11ef257c
nmalicki <nmalicki@CompSciICv-10.fsc.int>
2024-03-07 15:50:26 -0500

    added scripts file and rearranged stuff

--- customer/signInPopUp.php
+++ customer/signInPopUp.php
@@ -1,35 +1,45 @@
+<?php
+
+/* 
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Scripting/EmptyPHP.php to edit this template
+ */
+
+?>
+
+
 <button id="signInContainerClose" onclick="closePopUp('signInContainer')">X</button>
 <div id="toggleDiv">
     <button id="registerButton">Register</button>
-    <button id="signInHButton">Sign In</button>
+    <button id="signInFormButton">Sign In</button>
 </div>
 
-<form id="registerForm">
+<form id="registerForm" action="registerAction.php" method="POST">
     <div id="registerNames">
         <div class="half left">
             <label>Given Name</label>
             <br>
-            <input></input>
+            <input name="givenName"></input>
         </div>
         <div class="half right">
             <label>Family Name</label>
             <br>
-            <input></input>
+            <input name="familyName"></input>
         </div>
     </div>    
     <label>Email</label>
     <br>
-    <input></input>
+    <input name="email"></input>
     <br>
     <label>Password</label>
     <br>
-    <input type="password"></input>
+    <input type="password" name="password"></input>
     <br>
     <label>Confirm password</label>
     <br>
     <input type="password"></input>
     <br>
-    <input type="submit"></input>
+    <input type="submit" class="submitButton"></input>
 </form>
 
 <form id="logInForm">
@@ -42,15 +52,10 @@
     <input></input>
     <br>
     <br>
-    <input type="submit"></input>
+    <input type="submit" class="submitButton"></input>
 </form>
 
 
 
-<?php
 
-/* 
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Scripting/EmptyPHP.php to edit this template
- */
 
--- header.php
+++ header.php
@@ -5,12 +5,12 @@
 
 <nav id="navBar">
     <div id="contextMenu">
-        <h1><a href="">Restaurant</a></h1>
+        <h1><a href="index.php">Restaurant</a></h1>
         <button>Test 1</button>
         <button>Test 2</button>
     </div>
     <div id="signInIsland">
-        <button><a href="customer/locations.php">Locations</a></button>
+        <button><a href="locations.php">Locations</a></button>
         <button id="signInButton" onclick="signInPopUp()">Sign In</button>
         <button id="signOut"> Sign Out</button>
     </div>
