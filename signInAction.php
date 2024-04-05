f98754088e86a593f1ccde14f6d7ba208e1c3c7b
nmalicki <nmalicki@CompSciICv-19.fsc.int>
2024-04-05 17:42:55 -0400

    fixed login

--- signInAction.php
+++ signInAction.php
@@ -1,21 +1,44 @@
 <?php
+
 include "header.php";
 require "shared/dbConnect.php";
 
 $email = $_POST["email"];
 $password = $_POST["password"];
 
-$sql = "SELECT `customerId`, `email`, `password`, `givenName`, `familyName`, `paymentInfo` FROM `customer` WHERE `email` = ? and `password` = ?";
+$sql = "SELECT * FROM `customer` WHERE `email` = '$email';";
 
-try {
-    //not working
-    $result = loginDB($sql, $email, $password);
-    //echo $result[1], $result[2];
-    echo "<h1 class= 'signInFeedback'>Sign In Successful</h1>";   
-    
-    
-    //echo "<a class='logLink backLink' href='index.php'>< Home</a>";
-} catch (Exception $e) {
-    $error = $e->getMessage();
-    echo "Other error: $error \n";
+$result = queryDB($sql);
+
+if ($result->num_rows == 1) {
+    if ($row = $result->fetch_assoc()) {
+        $result = array($row["customerId"], $row["email"], $row["password"], $row["givenName"], $row["familyName"], $row["paymentInfo"]);
+        if (password_verify($password, $row["password"])) {
+            //ui for user after successfull login
+            echo "<h1 class='logInFeedback'>Log In Successful</h1>";
+            echo "<a class='logLink backLink' href='index.php'>< Home</a>";
+
+            /*
+              //setting session values
+              $_SESSION['usertype'] = $row["userType"];
+              $_SESSION['favoriteTeam'] = $row["favoriteTeam"];
+              $_SESSION['userInfo'] = $result;
+
+              //converting session values to json
+              $exportType = json_encode($_SESSION["usertype"]);
+              $exportUserInfo = json_encode($_SESSION["userInfo"]);
+              $exportTeam = json_encode($_SESSION["favoriteTeam"]);
+
+              //make json visible to js
+              echo "<script>usertype = $exportType;  sessionStorage.setItem('userInfo', $exportUserInfo); loggedIn();</script>";
+             * 
+             */
+        } else {
+            echo "<h1 class='logInFeedback'>Invalid username or password.</h1>";
+            echo "<a class='logLink backLink' href='index.php'>< Back</a>";
+        }
+    }
+} else {
+    echo "<h1 class='logInFeedback'>Username or password is wrong </h1>";
+    echo "<a class='logLink backLink' href='index.php'>< Back</a>";
 }
